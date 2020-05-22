9a78c311fab870469944256bcb3406926e6c0bcb
daryn <daryn@hex50.com>
2020-05-22 15:29:21 -0700

    add submit, postgres, email

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-#### FlaskTut -- flask tutorial from the docs
+#### FlaskTut -- flask tutorial Brad Traversy - 08/2019
+
+  * build & deploy
+  * mailtrap.io
+  * https://picsum.photos/images
+  * postgresql: 
+    - `sudo apt install postgresql postgresql-contrib`
+    - `sudo apt install libpq-dev`
+    - `pip3 install psycopg2`
+    - `pip3 install psycopg2-binary`
+    - `pip3 install flask-sqlalchemy`
+    - `pip3 install gunicorn`
+
---
+++ send_mail.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = '4163c0b9c7dadc'
+    password = '4c95b1efb7878a'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'stormy@bogus.net'
+    receiver_email = 'forrest@bogus.net'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexus Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    # send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
